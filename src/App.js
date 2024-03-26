7e458aabe390c063e23bd10dcb0f028a62c85d29
ortis88 <caesar0718@gmail.com>
2024-03-26 15:03:08 +0800

    Demo day2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
+//項目的根組件
+//被index.js引入->public/index.html渲染
+
+//count是狀態變數
+//setCount 修改狀態變數方法
 function App() {
+  const[count,setCount]=useState(0)
+
+  function handleCount(){
+    setCount(count+1)
+  }
+
+  const[form,setForm]=useState({name:'Alex'});
+
+  function handleForm(){
+    setForm(
+      {...form,name:'John'}
+    )
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleCount}>{count}</button>
+      <button onClick={handleForm}>修改為{form.name}</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
+//導入React的核心功能
 import React from 'react';
+//導入ReactDOM庫的核心功能
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+//導入項目的根組件
+import App from './App';
+
+
+//把APP根組件渲染到id="root"的dom節點上,在public中的index.html
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
